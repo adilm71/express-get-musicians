0a7a2415980cca6ccbcecbcc2094ad0f43e1c7e7
adilm71 <146095499+adilm71@users.noreply.github.com>
2023-11-06 16:14:58 +0000

    adding GET request and tests@

--- index.test.js
+++ index.test.js
@@ -1,22 +1,23 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  test("Testing musicians endpoint", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
+  test("Testing if data is sent", async () => {
+    const response = await request(app).get("/musicians");
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+    const responseData = JSON.parse(response.text);
+    expect(responseData).toBe(responseData);
+  });
+});
